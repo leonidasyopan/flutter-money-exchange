cafa31e3c2d4ab476058cf0600012cb6f2537eca
Leonidas Yopan <leonidasyopan@gmail.com>
2020-09-04 13:50:13 -0300

    started homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:money_exchange/src/pages/homepage.dart';
+import 'package:money_exchange/src/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: ApplicationTheme(context),
       home: Homepage(),
     );
   }
--- lib/src/pages/homepage.dart
+++ lib/src/pages/homepage.dart
@@ -1,10 +1,147 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Color(0xFFF1F5F8),
+      body: Column(
+        children: [
+          mainMenu(),
+          SizedBox(
+            height: 10.0,
+          ),
+          wallets(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history), title: Text('History')),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person), title: Text('Profile')),
+        ],
+        fixedColor: Color(0xFF3153F5),
+        onTap: null,
+      ),
     );
   }
+}
+
+Widget mainMenu() {
+  return Container(
+    height: 236.0,
+    decoration: BoxDecoration(
+        gradient: LinearGradient(
+      colors: [Color(0xFF2848FF), Color(0xFF3969FC)],
+      stops: [0.0, 0.7],
+    )),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '\$27,812.',
+              style: TextStyle(
+                  fontSize: 35.0,
+                  color: Color(0xFFFFFFFF),
+                  fontWeight: FontWeight.w500),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 6.0),
+              child: Text(
+                '34',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Color(0xFFFFFFFF),
+                    fontWeight: FontWeight.w500),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            menuOption(Icons.arrow_downward, 'Receive', 45),
+            menuOption(Icons.arrow_upward, 'Send', 45),
+            menuOption(Icons.compare_arrows, 'Exchange', 0),
+            menuOption(Icons.attach_money, 'Cash out', 0)
+          ],
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+      ],
+    ),
+  );
+}
+
+Widget menuOption(IconData icon, String title, int angle) {
+  return Column(
+    children: [
+      ClipOval(
+        child: Material(
+          color: Color(0xFF4F77FC),
+          child: InkWell(
+            splashColor: Colors.white,
+            child: SizedBox(
+                width: 70,
+                height: 70,
+                child: Transform.rotate(
+                  angle: angle * pi / 180,
+                  child: Icon(
+                    icon,
+                    size: 35,
+                    color: Colors.white,
+                  ),
+                )),
+            onTap: () {},
+          ),
+        ),
+      ),
+      SizedBox(height: 8.0),
+      Text(
+        title,
+        style: TextStyle(
+          color: Color(0xFFFFFFFF),
+          fontSize: 14.0,
+          fontWeight: FontWeight.w500,
+        ),
+      )
+    ],
+  );
+}
+
+Widget wallets() {
+  return Container(
+    height: 200.0,
+    decoration: BoxDecoration(
+        gradient: LinearGradient(
+      colors: [Color(0xFFFFFFFF), Color(0xFFFFFFFF)],
+      stops: [0.0, 0.7],
+    )),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text('My wallets'),
+            Text('Empty walltes'),
+          ],
+        )
+
+      ],
+    )
+  );
 }

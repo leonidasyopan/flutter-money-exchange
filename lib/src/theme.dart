1726819d5759cc3b3bf9bd878030d0439dcffec0
Leonidas Yopan <leonidasyopan@gmail.com>
2020-09-04 13:49:55 -0300

    creatred a theme file

---
+++ lib/src/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData ApplicationTheme(BuildContext context) {
+  return Theme.of(context).copyWith(
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    textTheme: Theme.of(context).textTheme.apply(fontFamily: 'Poppins'),
+    primaryTextTheme: Theme.of(context).textTheme.apply(fontFamily: 'Poppins'),
+    accentTextTheme: Theme.of(context).textTheme.apply(fontFamily: 'Poppins'),
+  );
+}
